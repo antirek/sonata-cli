8a936d60e81ea4a37a049aefc2fa14e2da46aaef
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-06-11 17:20:54 +0700

    добавил вывод в таблицу списка вендоров

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const program = require('commander');
 const fetch = require('node-fetch');
+const table = require('tty-table');
 
-const sonataManageApi = 'http://localhost:3021/v1';
+const sonataManageApi = 'http://sonata_management_api:3021/v1';
 
 program
   .version('0.1.0');
 
 program
   .command('vendors')
-  // .option('-p, --peppers', 'Add peppers')
   .action((cmd, env) => {
     fetch(sonataManageApi + '/vendor')
       .then(res => res.json())
-      .then(console.log);
+      .then((res) => {
+        const header = [{value: 'id'}, {value: 'name'}];
+        //console.log(res);
+        const rows = res.map((vendor) => {
+          return [vendor.id, vendor.name ];
+        });
+        const options = {};
+        let t1 = table(header, rows, options);
+        console.log(t1.render()); 
+      });
   });
+
+
+program
+  .command('vendor <id>')  
+  .action((cmd, env) => {
+    // console.log(cmd);
+    fetch(sonataManageApi + '/vendor/' + cmd)
+      .then(res => res.json())
+      .then(console.log)
+      .catch((err) => {
+        console.log('err', err);
+      })
+  });
+
 
 program.parse(process.argv);
